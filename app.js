81012e05d2fa78208ff7a85348f83c4a6822d040
Marc319-jr <marcelo.sznek@gmail.com>
2021-04-12 20:29:26 -0300

    Apertura sprint 3

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const color = require('colors');
+const path = require('path');
+const app = express();
+
+
+//Config
+app.use(express.static('public'));
+
+//Rutas
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/product.html'));
+});
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/carrito.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+
+//Levantamos servidor y por si nos dan un puerto
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'));
+console.log("Server on port".trap.random, app.get('port'));
